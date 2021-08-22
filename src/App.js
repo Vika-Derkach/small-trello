b629ba0b24b9b66151c067398415f4b325f13032
darkander <kingdarkander@gmail.com>
2021-08-22 21:38:06 +0300

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cardList, setCardList] = useState([
+    { id: 1, order: 3, text: "Card 3" },
+    { id: 2, order: 1, text: "Card 1" },
+    { id: 3, order: 2, text: "Card 2" },
+    { id: 4, order: 4, text: "Card 4" },
+  ]);
+  const [currentCard, setCurrentCard] = useState(null);
+  const dragStartHandler = (e, card) => {
+    console.log("drag", card);
+    setCurrentCard(card);
+  };
+  const dragLeaveHandler = (e) => {};
+  const dragEndHandler = (e) => {
+    e.target.style.background = "white";
+  };
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+    e.target.style.background = "lightgray";
+  };
+  const dropHandler = (e, card) => {
+    e.preventDefault();
+    console.log("drop", card);
+
+    setCardList(
+      cardList.map((c) => {
+        if (c.id === card.id) {
+          return { ...c, order: currentCard.order };
+        }
+        if (c.id === currentCard.id) {
+          return { ...c, order: card.order };
+        }
+        return c;
+      })
+    );
+    e.target.style.background = "white";
+  };
+  const sortCards = (a, b) => {
+    if (a.order > b.order) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {cardList.sort(sortCards).map((card) => {
+        return (
+          <div
+            key={card.id}
+            onDragStart={(e) => dragStartHandler(e, card)}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragEnd={(e) => dragEndHandler(e)}
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropHandler(e, card)}
+            draggable={true}
+            className={"card"}
+          >
+            {card.text}
+          </div>
+        );
+      })}
     </div>
   );
 }
